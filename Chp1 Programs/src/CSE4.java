25e3a0d6e9191c65204f207c59bb2a45b9494449
aadityakm113 <aadityakm113@gmail.com>
2023-04-05 02:41:07 +0530

    commit

---
+++ Chp1 Programs/src/CSE4.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class CSE4 {
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int x= sc.nextInt();
+
+        switch(x)
+        {
+            case 1:
+                System.out.println("AAA");
+                break;
+            case 2:
+                System.out.println("BAA");
+                break;
+            case 3:
+                System.out.println("CAA");
+                break;
+            default:
+                System.out.println("JAVA");
+                break;
+        }
+            
+        sc.close();
+
+
+
+    }
+}
